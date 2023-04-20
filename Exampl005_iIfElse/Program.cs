49921565e480c40a1b37aa4fbb396a08968453ff
valmar98@mail.ru <valmar98@mail.ru>
2023-04-20 16:52:08 +0300

    add Exampl005 if else

---
+++ Exampl005_iIfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "marat")
+{
+
+    Console.WriteLine("Ура это хозяин");
+
+}
+else
+{
+    Console.WriteLine("Привет");
+    Console.WriteLine(username);
+
+}
